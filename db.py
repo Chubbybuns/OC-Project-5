d05af846042c5935e1be8c522456df333b172f2d
Julia <julia.l171199@gmail.com>
2020-08-29 12:28:23 +0200

    mise à jour variables d'environnement

--- db.py
+++ db.py
@@ -1,5 +1,10 @@
 from peewee import MySQLDatabase
-from secrets import user, password, db_name, host
+# from secrets import user, password, db_name, host
+import os
+user = os.environ.get("USER")
+password = os.environ.get("PASSWORD")
+db_name = os.environ.get("DB_NAME")
+host = os.environ.get("HOST")
 
 mysql_db = MySQLDatabase(db_name, user=user, password=password,
                          host=host, port=3306)
